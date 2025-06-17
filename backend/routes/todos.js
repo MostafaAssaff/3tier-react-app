94e2b5fb4ac30877e73e5ab4e7c77efdc329ca7e
MostafaAssaff <mostafaassaaff@gmail.com>
2025-06-17 19:51:14 +0300

    Update todos.js

--- backend/routes/todos.js
+++ backend/routes/todos.js
@@ -1,66 +1,119 @@
 const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 const router = express.Router();
+const { dynamodb, TABLE_NAME } = require("../models/todo");
 
-const Todo = require("../models/todo");
-
-// GET all todos
+// GET all incomplete todos
 router.get("/", async (req, res) => {
-  const todos = await Todo.find({ is_complete: false });
-  res.send(todos);
+  const params = {
+    TableName: TABLE_NAME,
+    FilterExpression: "is_complete = :val",
+    ExpressionAttributeValues: {
+      ":val": false
+    }
+  };
+
+  try {
+    const data = await dynamodb.scan(params).promise();
+    res.send(data.Items);
+  } catch (err) {
+    res.status(500).send({ error: "Failed to fetch todos" });
+  }
 });
 
-// GET todo based on ID
+// GET todo by ID
 router.get("/:id", async (req, res) => {
-  const todo = await Todo.findOne({ _id: req.params.id });
-  res.send(todo);
+  const params = {
+    TableName: TABLE_NAME,
+    Key: {
+      id: req.params.id
+    }
+  };
+
+  try {
+    const data = await dynamodb.get(params).promise();
+    if (!data.Item) return res.status(404).send({ error: "Not found" });
+    res.send(data.Item);
+  } catch (err) {
+    res.status(500).send({ error: "Failed to fetch todo" });
+  }
 });
 
-// POST create new todo
+// CREATE new todo
 router.post("/", async (req, res) => {
-  console.log(req.body);
-  const todo = new Todo({
+  const todo = {
+    id: uuidv4(),
     title: req.body.title,
     description: req.body.description,
     is_complete: req.body.is_complete || false,
-    due_date: req.body.due_date || new Date(),
-  });
-  await todo.save();
-  res.send(todo);
+    due_date: req.body.due_date || new Date().toISOString()
+  };
+
+  const params = {
+    TableName: TABLE_NAME,
+    Item: todo
+  };
+
+  try {
+    await dynamodb.put(params).promise();
+    res.send(todo);
+  } catch (err) {
+    res.status(500).send({ error: "Failed to create todo" });
+  }
 });
 
 // UPDATE todo
 router.patch("/:id", async (req, res) => {
-  try {
-    const todo = await Todo.findOne({ _id: req.params.id });
+  const { title, description, is_complete, due_date } = req.body;
 
-    if (req.body.title) {
-      todo.title = req.body.title;
-    }
-    if (req.body.description) {
-      todo.description = req.body.description;
-    }
-    if (req.body.is_complete) {
-      todo.is_complete = req.body.is_complete;
-    }
-    if (req.body.due_date) {
-      todo.due_date = req.body.due_date;
-    }
-    await todo.save();
-    res.send(todo);
-  } catch {
-    res.status(404);
-    res.send({ error: "Todo does not exist!" });
+  const updateExpressions = [];
+  const expressionAttributeValues = {};
+
+  if (title !== undefined) {
+    updateExpressions.push("title = :title");
+    expressionAttributeValues[":title"] = title;
+  }
+  if (description !== undefined) {
+    updateExpressions.push("description = :description");
+    expressionAttributeValues[":description"] = description;
+  }
+  if (is_complete !== undefined) {
+    updateExpressions.push("is_complete = :is_complete");
+    expressionAttributeValues[":is_complete"] = is_complete;
+  }
+  if (due_date !== undefined) {
+    updateExpressions.push("due_date = :due_date");
+    expressionAttributeValues[":due_date"] = due_date;
+  }
+
+  const params = {
+    TableName: TABLE_NAME,
+    Key: { id: req.params.id },
+    UpdateExpression: "set " + updateExpressions.join(", "),
+    ExpressionAttributeValues: expressionAttributeValues,
+    ReturnValues: "ALL_NEW"
+  };
+
+  try {
+    const result = await dynamodb.update(params).promise();
+    res.send(result.Attributes);
+  } catch (err) {
+    res.status(500).send({ error: "Failed to update todo" });
   }
 });
 
 // DELETE todo
 router.delete("/:id", async (req, res) => {
+  const params = {
+    TableName: TABLE_NAME,
+    Key: { id: req.params.id }
+  };
+
   try {
-    await Todo.deleteOne({ _id: req.params.id });
+    await dynamodb.delete(params).promise();
     res.status(204).send();
-  } catch {
-    res.status(404);
-    res.send({ error: "Todo does not exist!" });
+  } catch (err) {
+    res.status(500).send({ error: "Failed to delete todo" });
   }
 });
 
